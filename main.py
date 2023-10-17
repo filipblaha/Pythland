870c873882bcb677a2e641aa7ef6c9efec4b7972
filipblaha <blahaf@gapps.zcu.cz>
2023-10-17 15:50:00 +0200

    ovládání keys

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ class Screen:
         self.width = width
         self.height = height
         self.name = name
-        self.display = pygame.display.set_mode((self.width, self.height))
-        pygame.display.toggle_fullscreen()
+        self.display = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN | pygame.SCALED)
         pygame.display.set_caption(self.name)
 
 
@@ -23,14 +22,14 @@ class Object:
         self.y = y
         self.speed = speed
 
-    def movement(self, key1, key2, key3, key4):
-        if key1:
+    def movement(self, keys):
+        if keys[pygame.K_a]:
             self.x -= self.speed
-        if key2:
+        if keys[pygame.K_d]:
             self.x += self.speed
-        if key3:
+        if keys[pygame.K_w]:
             self.y -= self.speed
-        if key4:
+        if keys[pygame.K_s]:
             self.y += self.speed
 
 
@@ -50,23 +49,26 @@ text = Text("Arial", 36)
 clock = pygame.time.Clock()
 
 start_time = time.time()
-game_over = False
 run = True
 while run:
 
+    # input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    # logic
+    player.movement(pygame.key.get_pressed())
+
     # FPS
     clock.tick(140)
-    keys = pygame.key.get_pressed()
 
     screen.display.fill((0, 0, 0))
-    player.movement(keys[pygame.K_a], keys[pygame.K_d], keys[pygame.K_w], keys[pygame.K_s])
-    # screen.display.blit(puzzle_window.sprite, (puzzle_window.x, puzzle_window.y))
     pygame.draw.rect(screen.display, (255, 255, 255), (550, 50, 840, 750), 5)
     screen.display.blit(player.sprite, (player.x, player.y))
-
     text.render(str(player.x), 200, 200)
     text.render(str(player.y), 200, 240)
 
