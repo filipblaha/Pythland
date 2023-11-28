d30692ce2d53db005f8f00f604c77b9b84927cf6
Matyáš Makrlík <matyas.makrlik@seznam.cz>
2023-11-28 16:10:43 +0100

    hranice s okraji

--- level.py
+++ level.py
@@ -3,6 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from  support import *
 
 class Level:
     def __init__(self):
@@ -17,10 +18,18 @@ class Level:
         #sprite setup
         self.creat_map()
     def creat_map(self):
-        # for row_index, row in enumerate(WORLD_MAP):
-        #     for col_index, col in enumerate(row):
-        #         x = col_index * TILESIZE
-        #         y = row_index * TILESIZE
+        layout = {
+                'boundary': import_csv_layout('map/zelda_FloorBlocks.csv')
+        }
+
+        for style,layout in layout.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),[self.visible_sprites,self.obstacle_sprite],'invinsible')
         #         if col == '221':
         #             Tile((x,y),[self.visible_sprites, self.obstacle_sprite])
         #         if col == 'p':
