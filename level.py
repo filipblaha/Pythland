976533b7aa6ade2cbbdb6383b1a057cb825c848b
Matyáš Makrlík <matyas.makrlik@seznam.cz>
2023-11-28 12:24:01 +0100

    background

--- level.py
+++ level.py
@@ -17,14 +17,16 @@ class Level:
         #sprite setup
         self.creat_map()
     def creat_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == '221':
-                    Tile((x,y),[self.visible_sprites, self.obstacle_sprite])
-                if col == 'p':
-                    self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprite)
+        # for row_index, row in enumerate(WORLD_MAP):
+        #     for col_index, col in enumerate(row):
+        #         x = col_index * TILESIZE
+        #         y = row_index * TILESIZE
+        #         if col == '221':
+        #             Tile((x,y),[self.visible_sprites, self.obstacle_sprite])
+        #         if col == 'p':
+        #             self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprite)
+        self.player = Player((195, 170), [self.visible_sprites], self.obstacle_sprite)
+
 
 
     def run(self):
@@ -42,11 +44,19 @@ class YSortCameraGroup (pygame.sprite.Group):
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
+
+    #     vytvareni podlahy
+        self.floor_surf = pygame.image.load('graphic/background.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
     def custum_draw(self,player):
 
         # ziskani offsetu
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # vykreslovani podlahy
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf,floor_offset_pos)
 
         for sprite in sorted(self.sprites(),key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
